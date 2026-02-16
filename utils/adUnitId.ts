8c3ae7deb172b47d7d2b900a2f58a6adcb65845b
Mateus Santos <mateuspele2015@gmail.com>
2026-02-16 14:00:40 -0300

    feat: update dependencies and improve ad unit ID handling

--- utils/adUnitId.ts
+++ utils/adUnitId.ts
@@ -1,7 +1,8 @@
+import { Platform } from "react-native";
 import { TestIds } from "react-native-google-mobile-ads";
 
-export const adUnitId = __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-6242824020711835/2624690184';
-export const bannerAdUnitId = __DEV__ ? TestIds.BANNER : 'ca-app-pub-6242824020711835/7440535619';
-export const bannerAdUnitId2 = __DEV__ ? TestIds.BANNER : 'ca-app-pub-6242824020711835~1164268856';
-export const rewardedInterstitialAdUnitId = __DEV__ ? TestIds.REWARDED_INTERSTITIAL : 'ca-app-pub-6242824020711835/2562421488';
-export const rewardedInterstitialAdUnitIdTwo = __DEV__ ? TestIds.REWARDED_INTERSTITIAL : 'ca-app-pub-6242824020711835/4637920480';
+export const adUnitId = __DEV__ ?  TestIds.INTERSTITIAL : Platform.OS === 'android' ? 'ca-app-pub-6242824020711835/2624690184' : 'ca-app-pub-6242824020711835/4227622528';
+export const bannerAdUnitId = __DEV__ ? TestIds.BANNER : Platform.OS === 'android' ?  'ca-app-pub-6242824020711835/7440535619' : 'ca-app-pub-6242824020711835/9490288579';
+export const bannerAdUnitId2 = __DEV__ ? TestIds.BANNER : Platform.OS === 'android' ? 'ca-app-pub-6242824020711835/9356709134' : 'ca-app-pub-6242824020711835/9490288579';
+export const rewardedInterstitialAdUnitId = __DEV__ ? TestIds.REWARDED_INTERSTITIAL : Platform.OS === 'android' ? 'ca-app-pub-6242824020711835/2562421488' : 'ca-app-pub-6242824020711835/4227622528';
+export const rewardedInterstitialAdUnitIdTwo = __DEV__ ? TestIds.REWARDED_INTERSTITIAL : Platform.OS === 'android' ? 'ca-app-pub-6242824020711835/4637920480' : 'ca-app-pub-6242824020711835/4227622528';
